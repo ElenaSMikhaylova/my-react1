bed16d38eda52d5cbbf08b8d2e9d25fba2d2d814
Elena.s.mikhaylova <elena.s.mikhaylova@gmail.com>
2021-09-15 20:19:18 +0300

    commit1

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,33 @@
-
-import { BrowserRouter } from 'react-router-dom';
-import './App.css';
+import { BrowserRouter, NavLink, Route } from 'react-router-dom';
+const Profile=()=>{return <h2> Профиль</h2>;};
+const Messages=()=>{return <h2> Сообщения</h2>;};
+const Settings=()=>{return <h2> Настройки</h2>;};
+const Frends=()=>{return <h2> Друзья</h2>;};
 
 function App() {
   return (
-    <div className="container-fluid">
-<div className="row">
+<div className="container-fluid">
+<BrowserRouter>
+<div className="row mt-5">
 <div className="col-3">
 <div className="nav flex-column nav-pills" id="v-pills-tab" >
-<a className="nav-link" href="profile" >Профиль</a>
-<a className="nav-link"  href="messages" >Сообщения</a>
-<a className="nav-link"  href="settings">Настройки</a>
-<a className="nav-link " href="frends" >Мои друзья</a>
+<NavLink className="nav-link" to="profile" >Профиль</NavLink>
+<NavLink className="nav-link"  to="messages" >Сообщения</NavLink>
+<NavLink className="nav-link"  to="settings">Настройки</NavLink>
+<NavLink className="nav-link " to="frends" >Мои друзья</NavLink>
 </div>
 </div>
 <div className="col-9">
-<BrowserRouter>
 
+<Route path="/profile" component={Profile}/>
+<Route path="/messages" component={Messages}/>
+<Route path="/settings" component={Settings}/>
+<Route path="/frends" component={Frends}/>
+
+</div>
+</div>
 </BrowserRouter>
 </div>
-
-</div>
-  </div>
-   
   );
 }
-
 export default App;
